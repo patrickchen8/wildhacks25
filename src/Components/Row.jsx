e32a0864cf31b5530e76bf8886ab9260eb55d262
patrickchen8 <patrick.chen0459@gmail.com>
2025-04-05 22:39:59 -0500

    working

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,9 +1,29 @@
 import { FaRegCheckCircle } from "react-icons/fa";
 import { GiHealthNormal } from "react-icons/gi";
+import { ImSpinner6 } from "react-icons/im";
+
 
 
 const Row = ({crop, storageType, isHealthy, amount, harvestDate, sellByDate, lastUpdateDate, setIsOpen}) => {
 
+    if(isHealthy === 'processing') {
+        var healthyCol = <div className="w-[200px] flex justify-center items-center">
+                        <ImSpinner6 />
+                        <p className="ml-2">processing</p> 
+                    </div>
+    }
+    else if(isHealthy)  {
+        var healthyCol =  <div className="w-[200px] flex justify-center items-center"> 
+                            <FaRegCheckCircle color="green"/> 
+                            <p className="ml-2">Healthy</p> 
+                        </div> 
+    }
+    else {
+        var healthyCol = <div className="w-[200px] flex justify-center items-center"> 
+                            <GiHealthNormal color="red"/> 
+                            <p className="ml-2">Unhealthy</p> 
+                        </div> 
+    }
 
     return (
         <div className="flex justify-between border-t-1 border-gray-300 py-2 bg-white cursor-pointer hover:bg-tan"
@@ -16,17 +36,7 @@ const Row = ({crop, storageType, isHealthy, amount, harvestDate, sellByDate, las
                 {storageType} 
             </div>
 
-            {
-                isHealthy ? <div className="w-[200px] flex justify-center items-center"> 
-                                <FaRegCheckCircle color="green"/> 
-                                 <p className="ml-2">Healthy</p> 
-                            </div> 
-                            : 
-                            <div className="w-[200px] flex justify-center items-center"> 
-                                <GiHealthNormal color="red"/> 
-                                 <p className="ml-2">Unhealthy</p> 
-                            </div> 
-            }
+            {healthyCol}
 
             <div className="w-[200px] text-center"> 
                 {amount} 
